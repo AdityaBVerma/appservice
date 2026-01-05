53642b8559d6318238224f539bc3adfa21f1f91d
AdityaBVerma <adityabverma1@gmail.com>
2026-01-05 12:05:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("🚀 Express server is running on Node 20");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
